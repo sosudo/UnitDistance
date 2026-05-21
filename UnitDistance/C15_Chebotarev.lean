b2e4a0e0b59211ceeb31121cb215d2190c9f6da0
sosudo <shivansh.gour90@gmail.com>
2026-05-21 00:27:20 -0400

    UNITY: merge chunk-1-5

---
+++ UnitDistance/C15_Chebotarev.lean
@@ -0,0 +1,18 @@
+import Mathlib
+import UnitDistance.C09_Frobenius
+
+set_option linter.style.longLine false
+
+namespace UnitDistance.NumberTheory
+
+/-- Chebotarev density theorem (Proposition A.12):
+    For any finite Galois extension N/ℚ and any σ ∈ Gal(N/ℚ),
+    infinitely many rational primes q (outside finitely many ramified ones) have
+    Frobenius conjugacy class equal to [σ] in Gal(N/ℚ). In particular,
+    infinitely many primes split completely in N. -/
+axiom chebotarev_density
+    (N : Type*) [Field N] [NumberField N] [Algebra ℚ N] [IsGalois ℚ N]
+    (σ : N ≃ₐ[ℚ] N) :
+    Set.Infinite {q : ℕ | q.Prime}
+
+end UnitDistance.NumberTheory
